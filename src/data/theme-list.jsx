cbfa1496e7e05805faea3da4481b17aef83a094e
Poap <alapetite.mathieu@gmail.com>
2024-11-15 11:44:02 -0500

    Update theme-list.jsx

--- src/data/theme-list.jsx
+++ src/data/theme-list.jsx
@@ -18,50 +18,54 @@ export const themeEn = [
 ]
 
 export const themeFr = [
+	
+	
 	{
-		"theme": "performance",
-		"categories": ["éloquence", "performance", "persuasion"]
+		"theme": "combat",
+		"categories": ["mouvement", "combat", "physique"]
 	},
 	{
 		"theme": "lutte",
 		"categories": ["physique", "lutte", "mouvement"]
 	},
 	{
-		"theme": "artisanat",
-		"categories": ["savoir", "artisanat", "science"]
+		"theme": "performance",
+		"categories": ["éloquence", "performance", "persuasion"]
 	},
 	{
-		"theme": "combat",
-		"categories": ["mouvement", "combat", "physique"]
-	},
-	{
-		"theme": "soin",
-		"categories": ["survie", "soin", "science"]
-	},
-	{
-		"theme": "magie",
-		"categories": ["minutie", "magie", "savoir"]
-	},
-	{
-		"theme": "ésotérisme",
-		"categories": ["perception", "ésotérisme", "détection"]
-	},
-	{
-		"theme": "défense",
-		"categories": ["logique", "défense", "physique"]
-	},
-	{
-		"theme": "négoce",
-		"categories": ["persuasion", "négoce", "logique"]
+		"theme": "tir",
+		"categories": ["minutie", "tir", "discrétion"]
 	},
 	{
 		"theme": "survie",
 		"categories": ["perception", "survie", "discrétion"]
 	},
 	{
-		"theme": "tir",
-		"categories": ["minutie", "tir", "discrétion"]
+		"theme": "défense",
+		"categories": ["logique", "défense", "physique"]
 	},
+	{
+		"theme": "ésotérisme",
+		"categories": ["perception", "ésotérisme", "détection"]
+	},
+	{
+		"theme": "magie",
+		"categories": ["minutie", "magie", "savoir"]
+	},	
+	{
+		"theme": "artisanat",
+		"categories": ["savoir", "artisanat", "science"]
+	},
+	{
+		"theme": "soin",
+		"categories": ["survie", "soin", "science"]
+	},
+	{
+		"theme": "négoce",
+		"categories": ["persuasion", "négoce", "logique"]
+	},
+	
+	
 
 ]
 
